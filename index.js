374d864e2eb8681c62404331317556789fdc8bbd
md-ashik-max <ar1214697@gmail.com>
2024-07-17 15:19:34 +0600

    added get method for find admin

--- index.js
+++ index.js
@@ -71,6 +71,18 @@ async function run() {
             }
         });
 
+        // admin 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
         // Logout endpoint (optional)
         app.post('/users/logout', (req, res) => {
             // Here you could handle any server-side cleanup if necessary
